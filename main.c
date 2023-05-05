d48bfca4ffac4544739d1a79f6f7754e569997ef
Melina <mrequena@fue.undef.edu.ar>
2023-05-04 21:37:54 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+
+/*Un laboratorio produce 30 remedios que tiene codificados de 1 a 30 de los que se
+conoce el código, nombre y precio. El cliente encarga varios medicamentos. Se
+ingresa el nombre y la cantidad solicitada en el orden en que son requeridos por el
+cliente.
+Imprimir la factura ordenada por número de código con la siguiente información:
+código, cantidad, nombre, precio y el total a pagar.*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
